c3612acf1b2ed36f7fa5d21186eb5ab57fb9e387
kjazgar <klaudia.jazgar@gmail.com>
2021-06-14 13:55:52 +0200

    Metrics update.

--- metrics.py
+++ metrics.py
@@ -1,4 +1,10 @@
-from sklearn import  metrics
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+import pandas as pd
+
+from ada_boost import ada
+from dec_tree import dec_tree
+from extra_tree import extra_tree
 from reprocess_data import reprocess_data2
 from logistic import logistic
 from svm_linear import svm_linear
@@ -6,13 +12,18 @@ from svm_poly import svm_poly
 from svm_rbf import svm_rbf
 from random_forest import random_forest
 from knn import knn
-X_train, X_test, y_train, y_test = reprocess_data2(100)
+import matplotlib.pyplot as plt
+
+X_train, X_test, y_train, y_test = reprocess_data2()
 log = logistic(X_train, X_test, y_train, y_test)
 svm_l = svm_linear(X_train, X_test, y_train, y_test)
 svm_p = svm_poly(X_train, X_test, y_train, y_test)
 svm_r = svm_rbf(X_train, X_test, y_train, y_test)
 rf = random_forest(X_train, X_test, y_train, y_test)
 knn = knn(X_train, X_test, y_train, y_test)
+dec = dec_tree(X_train, X_test, y_train, y_test)
+extr = extra_tree(X_train, X_test, y_train, y_test)
+ad = ada(X_train, X_test, y_train, y_test)
 
 models = []
 models.append(('LR', log.best_estimator_))
@@ -20,35 +31,64 @@ models.append(('SVC linear', svm_l.best_estimator_))
 models.append(('SVC poly', svm_p.best_estimator_))
 models.append(('SVC rbf', svm_r.best_estimator_))
 models.append(('RandomForest', rf.best_estimator_))
-# models.append(('AdaBoost', grid_6.best_estimator_))
-# models.append(('GradientBoosting', grid_7.best_estimator_))
 models.append(('KNeighbours', knn.best_estimator_))
-# models.append(('DecisionTree', grid_9.best_estimator_))
-# models.append(('ExtraTrees', grid_10.best_estimator_))
+models.append(('DecisionTree', dec.best_estimator_))
+models.append(('ExtraTrees', extr.best_estimator_))
+models.append(('AdaBoost', ad.best_estimator_))
+
 
 precision_score = []
 recall_score = []
 f1_score = []
 accuracy_score = []
 for name, model in models:
-  y_pred = model.predict(X_test)
-  print(name)
-  print("precision_score: {}".format(metrics.precision_score(y_test, y_pred, average='micro')))
-  print("recall_score: {}".format(metrics.recall_score(y_test, y_pred, average='micro')))
-  print("f1_score: {}".format(metrics.f1_score(y_test, y_pred, average='micro')))
-  print("accuracy_score: {}".format(metrics.accuracy_score(y_test, y_pred)))
-  precision_score.append(metrics.precision_score(y_test, y_pred, average='micro'))
-  recall_score.append(metrics.recall_score(y_test, y_pred, average='micro'))
-  f1_score.append(metrics.f1_score(y_test, y_pred, average='micro'))
-  accuracy_score.append(metrics.accuracy_score(y_test, y_pred))
+    y_pred = model.predict(X_test)
+    print(name)
+    print("precision_score: {}".format(metrics.precision_score(y_test, y_pred, average='micro')))
+    print("recall_score: {}".format(metrics.recall_score(y_test, y_pred, average='micro')))
+    print("f1_score: {}".format(metrics.f1_score(y_test, y_pred, average='micro')))
+    print("accuracy_score: {}".format(metrics.accuracy_score(y_test, y_pred)))
+    print(confusion_matrix(y_test, y_pred))
+    precision_score.append(metrics.precision_score(y_test, y_pred, average='micro'))
+    recall_score.append(metrics.recall_score(y_test, y_pred, average='micro'))
+    f1_score.append(metrics.f1_score(y_test, y_pred, average='micro'))
+    accuracy_score.append(metrics.accuracy_score(y_test, y_pred))
 
 
 
-import pandas as pd
+
 d = {'precision_score': precision_score,
      'recall_score': recall_score,
      'f1_score': f1_score,
      'accuracy_score' : accuracy_score}
 df = pd.DataFrame(data=d)
-df.insert(loc=0, column='Method', value=['LR', 'SVC linear', 'SVC poly', 'SVC rbf', 'Random Forest', 'KNeighbours'])
+df.insert(loc=0, column='Method', value=['LR', 'SVC linear', 'SVC poly', 'SVC rbf', 'Random Forest', 'KNeighbours', 'DecisionTree', 'ExtraTrees', 'AdaBoost'])
 print(df)
+
+num = 1
+for mod in (log, svm_l):
+    print(num)
+    y_score = mod.fit(X_train, y_train).decision_function(X_test)
+    n_classes = 5
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        y_test_dummies = pd.get_dummies(y_test, drop_first=False).values
+        fpr[i], tpr[i], _ = roc_curve(y_test_dummies[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Plot of a ROC curve for a specific class
+    for i in range(n_classes):
+        plt.figure()
+        plt.plot(fpr[i], tpr[i], label='ROC curve (area = %0.2f)' % roc_auc[i])
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic example')
+        plt.legend(loc="lower right")
+        plt.show()
+
+    num = num + 1
